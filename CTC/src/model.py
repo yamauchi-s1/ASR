65e2e5d9af0ca733b81e0cbb3399b4626a524190
Shogo Yamauchi <yamauchi-s1@asahi.com>
2024-08-23 14:39:42 +0900

    add CTC model

--- CTC/src/model.py
+++ CTC/src/model.py
@@ -24,12 +24,45 @@ class CTCModel(nn.Module):
                 dim_enc_hid,
                 dim_enc_proj,
                 dim_out,
-                emc_num_layers,
+                enc_num_layers,
                 enc_bidirectional,
                 enc_sub_sample,
                 enc_rnn_type,
                 ):
         super(CTCModel, self).__init__()
         
-        self.encoder = Encoder()
+        self.encoder = Encoder(dim_in = dim_in,
+                                dim_hidden = dim_enc_hid,
+                                dim_proj = dim_enc_proj,
+                                num_layers = enc_num_layers,
+                                bidirectional = enc_bidirectional,
+                                sub_sample = enc_sub_sample,
+                                rnn_type = enc_rnn_type)
+        #出力層
+        self.out = nn.Linear(in_features=dim_enc_proj,
+                            out_features=dim_out)
+        
+        #重みを初期化
+        lecun_initialization(self)
+        
+        
+    def forward(self, input_sequence, input_lengths):
+        
+        '''
+        forward計算
+        --------------------------------------------------
+        input_sequence : 各発話の入力系列([ B x Tin x D])
+        input_lengths  : 各発話の系列長(フレーム数 [B])
+        
+        B   : ミニバッチ内の発話数
+        Tin : 入力テンソルの系列長(0 paddingを含める)
+        D   : 入力次元数(dim_in)
+        --------------------------------------------------
+        '''
+        
+        enc_out, enc_lenghts = self.encoder(input_sequence, input_lengths)
+        output = self.out(enc_out)
+        
+        return output, enc_lenghts
+        
         
